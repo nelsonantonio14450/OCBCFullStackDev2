94a81e6e295444dc0ba3b943fe3fd2957182ca87
Nelson Antonio <nelson.antonio14450@gmail.com>
2021-11-08 11:47:29 +0700

    sesi1

--- sesi1/intro.py
+++ sesi1/intro.py
@@ -1,3 +1,81 @@
-n = 300
+# n = 300
 
-print(n)
+# print(n)
+
+# print(100 < 200)
+
+# a = b = c = 4
+
+# print(a, b, c)
+
+# n = "mi"
+
+# print(type(n))
+
+# print(a * b + c)
+
+# print(4 // 3)
+
+# print(8 ** 4)
+
+# print(4 % 16)
+
+# print(a != b)
+
+# s = 'fOo'
+# t = 'bar'
+# # + and * Operators
+# print(s + t)
+# print(s * 4)
+# # Case Conversion
+# print(s.capitalize())
+# print(s.lower())
+# print(s.swapcase())
+
+# a = ['foo', 'bar', 'baz', 'qux']
+# print(a[0])
+# b = [20, 15, 1]
+# print(b[0])
+
+# event_a = ['bandung', 1140]
+# event_b = ['jakarta', 44500]
+
+# event_c = [event_a, event_b]
+# print(event_c[0][0])
+
+# print(len(a))
+
+# a = ['foo', 6, 7, 300, 10, 'bar', 'baz', 'qux']
+# print(a[0:5])
+
+# a = ['foo', 'bar', 'baz', 'qux', 'quux', 'corge']
+# print(a[1:4])
+# a[1:4] = [1.1, 2.2, 3.3, 4.4, 5.5]
+# print(a)
+
+# a.append(100)
+# print(a)
+
+# arr = [1, 8, 4, 15]
+# arr.sort()
+# print(arr)
+
+# kyk JSON
+# MLB_team = {'Colorado': 'Rockies', 'Boston': 'Red Sox', 'Minnesota': 'Twins', }
+# print(MLB_team['Minnesota'])  # Adding an entry to an existing dictionary
+# MLB_team['Kansas City'] = 'Royals'
+# print(MLB_team['Kansas City'])
+# MLB_team['KKK'] = ['ngggg', 'asdasdasd']
+# MLB_team['KKK'].append('asdadddddd')
+# print(MLB_team)
+
+person = {}
+person['fname'] = 'Hack'
+person['lname'] = '8'
+person['age'] = 51
+person['spouse'] = 'Edna'
+person['children'] = ['Ralph', 'Betty', 'Joey']
+person['pets'] = {'dog': 'Fido', 'cat': 'Sox'}
+print(person)
+print(person['children'][1])
+print(person['pets']['cat'])
